7bef2d5a2fd63b1b5ad74a0df121498ce2ed1034
Stephane Lucien-Vauthier <stephane.lucien-vauthier@pytech.fr>
2020-08-08 20:58:43 +0200

    RTemove batch list in product update and create

--- products/views.py
+++ products/views.py
@@ -3,10 +3,10 @@ from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from products.models import Product, Batch
-from products.serializers import ProductSerializer, ProductListSerializer, BatchSerializer
+from products.serializers import ProductSerializer, ProductUpdateSerializer, ProductListSerializer, BatchSerializer
 
 
-class ProductView(generics.ListCreateAPIView):
+class ProductView(APIView):
     """
     This class manages the view to create and list the products.
 
@@ -24,18 +24,49 @@ class ProductView(generics.ListCreateAPIView):
     """
 
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = ProductListSerializer
 
-    def get_queryset(self):
-        """Return the category list of the owner."""
-        return Product.objects.filter(owner=self.request.user)  # pylint: disable=no-member
+    def get(self, request, format=None):
+        """
+        Retrieve the product list with batches in stock.
 
-    def perform_create(self, serializer):
-        """Add the owner of the category before create it."""
-        serializer.save(owner=self.request.user)
+        Attributes:
+            request (Request): The request sent to the api.
+            format (NoneType): Always none, pass by Accept header.
+
+        Returns:
+            200: The product is updated.
+            400: An error is detected on the request data.
+            401: The user must be connected to access this resource.
+            406: The response format is not acceptable by the server.
+            500: An error was occured in the treatment of the request.
+        """
+        products = Product.objects.filter(owner=request.user) # pylint: disable=no-member
+        serializer = ProductListSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        """
+        Create a product.
+
+        Attributes:
+            request (Request): The request sent to the api.
+            format (NoneType): Always none, pass by Accept header.
+
+        Returns:
+            201: The product is created.
+            400: An error is detected on the request data.
+            401: The user must be connected to access this resource.
+            406: The response format is not acceptable by the server.
+            500: An error was occured in the treatment of the request.
+        """
+        serializer = ProductUpdateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProductDetail(APIView):
     """
     This class manages the view to update and delete a product.
 
@@ -53,15 +84,73 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     """
 
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        """Return the product of the owner."""
-        return Product.objects.filter(owner=self.request.user, pk=self.kwargs['pk'])  # pylint: disable=no-member
+    def get(self, request, pk, format=None):
+        """
+        Retrieve a batch.
 
-    def perform_update(self, serializer):
-        """Update the owner of the product before update it."""
-        serializer.save(owner=self.request.user)
+        Attributes:
+            request (Request): The request sent to the api.
+            pk (int): The product identifier.
+            format (NoneType): Always none, pass by Accept header.
+
+        Returns:
+            200: The product is updated.
+            400: An error is detected on the request data.
+            401: The user must be connected to access this resource.
+            406: The response format is not acceptable by the server.
+            500: An error was occured in the treatment of the request.
+        """
+        try:
+            product = Product.objects.get(owner=request.user, pk=pk) # pylint: disable=no-member
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        """
+        Update a product.
+
+        Attributes:
+            request (Request): The request sent to the api.
+            pk (int): The product identifier.
+            format (NoneType): Always none, pass by Accept header.
+
+        Returns:
+            200: The product is updated.
+            400: An error is detected on the request data.
+            401: The user must be connected to access this resource.
+            406: The response format is not acceptable by the server.
+            500: An error was occured in the treatment of the request.
+        """
+        product = Product.objects.get(owner=request.user, pk=pk) # pylint: disable=no-member
+        serializer = ProductUpdateSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        """
+        Delete a product.
+
+        Attributes:
+            request (Request): The request sent to the api.
+            pk (int): The product identifier.
+            ps (int): the batch identifier.
+            format (NoneType): Always none, pass by Accept header.
+
+        Returns:
+            204: The product is deleted.
+            400: An error is detected on the request data.
+            401: The user must be connected to access this resource.
+            406: The response format is not acceptable by the server.
+            500: An error was occured in the treatment of the request.
+        """
+        product = Product.objects.get(owner=request.user, pk=pk) # pylint: disable=no-member
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class BatchView(generics.ListCreateAPIView):

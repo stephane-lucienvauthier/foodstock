f52c50e44bd52a700993df3afb634c20bea151a4
Stephane Lucien-Vauthier <stephane.lucien-vauthier@pytech.fr>
2020-08-16 11:04:43 +0200

    Add batch

--- src/apps/products/interfaces.ts
+++ src/apps/products/interfaces.ts
@@ -1,24 +1,31 @@
-import { Product, ProductAdd } from './models'
+import { Product, ProductAdd, BatchAdd } from './models'
 import { Category } from '../categories/models'
+import { Provider } from '../providers/models'
 
 export interface ProductsProps {
     products: Product[]
     categories: Category[]
+    providers: Provider[]
     onAdd(product: ProductAdd): void
+    onBatchAdd(productId: number, batch: BatchAdd): void
 }
 
 export interface ProductsState {
     open: boolean
- }
+    batchAddOpen: boolean
+    current?: Product
+}
 
 export interface ProductListProps {
     products: Product[]
+    showBatchAddForm(product: Product) :void
 }
 
-export interface ProductListState {}
+export interface ProductListState { }
 
 export interface ProductListRowProps {
     product: Product
+    showBatchAddForm(product: Product) :void
 }
 
 export interface ProductListRowState {
@@ -34,4 +41,16 @@ export interface ProductAddFormProps {
 
 export interface ProductAddFormState {
     product: ProductAdd
+}
+
+export interface BatchAddFormProps {
+    open: boolean
+    providers: Provider[]
+    unit: string
+    onAdd(batch: BatchAdd): void
+    onCancel(): void
+}
+
+export interface BatchAddFormState {
+    batch: BatchAdd
 }

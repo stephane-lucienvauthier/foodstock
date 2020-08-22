c4fe5c14ea17cf44ebb14b8b676f151b9eb5f10f
Stephane Lucien-Vauthier <stephane.lucien-vauthier@pytech.fr>
2020-08-22 13:15:06 +0200

    Delete category

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -4,6 +4,7 @@ import Icon from '@material-ui/core/Icon'
 import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListSubheader from '@material-ui/core/ListSubheader'
 
@@ -19,7 +20,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function CategoryList(props) {
-  const { categories, onEditDialogOpen } = props
+  const { categories, onEditDialogOpen, onDeleteDialogOpen } = props
   const classes = useStyles();
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
@@ -32,7 +33,11 @@ export default function CategoryList(props) {
       <List component="nav" subheader={<ListSubheader component="div" id="nested-list-subheader">Categories<IconButton className={classes.addButton} color="primary" onClick={onEditDialogOpen}><Icon>add</Icon></IconButton></ListSubheader>}>
         <ListItem button selected={selectedIndex === 0} key="0" onClick={(event) => onChange(event, 0)}><ListItemText primary="All" /></ListItem>
         {categories !== undefined && categories.map(category => (
-          <ListItem button selected={selectedIndex === category.id} key={category.id} onClick={(event) => onChange(event, category.id)}><ListItemText primary={category.label} /></ListItem>
+          <ListItem button selected={selectedIndex === category.id} key={category.id} onClick={(event) => onChange(event, category.id)}><ListItemText primary={category.label} />
+            <ListItemSecondaryAction>
+              <IconButton size="small" onClick={() => onDeleteDialogOpen(category.id)} color="secondary"><Icon>delete</Icon></IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
         ))}
       </List>
     </div>
